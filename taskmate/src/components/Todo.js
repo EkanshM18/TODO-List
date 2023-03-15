43f2be92ffb4c0a15764cc31992096e5954ab2af
EKANSHMAHESHWARI18 <104378139+EKANSHMAHESHWARI18@users.noreply.github.com>
2023-03-15 23:48:25 +0530

    Initial Commit

---
+++ taskmate/src/components/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+// import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FaEdit,FaTrash } from 'react-icons/fa'
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+ 
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+            <FaEdit onClick={() => editTodo(task.id)} />
+            <FaTrash onClick={() => deleteTodo(task.id)} />
+        {/* <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} /> */}
+        {/* <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} /> */}
+        </div>
+    </div>
+  )
+}
